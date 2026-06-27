5d50f8a6138d0adf5a777a771148b2c91e887783
FireFx1 <gabrieltaraganodb@gmail.com>
2026-06-27 11:52:52 -0300

    V3
    Se integra el uso de archivo txt

--- v1.py
+++ v1.py
@@ -1,79 +1,72 @@
-class CajeroAutomatico:
+class cajeroAutomatico:
     def __init__(self):
+        with open("datos.txt", "r") as archivo:
+            lineas = archivo.readlines()
         # acceso
-        self.usuario = "admin"
-        self.contrasena = "1234"
+        self.usuario = lineas[0].strip()
+        self.contrasena = lineas[1].strip()
 
         # datos de cuenta
-        self.saldo = 100000
-        self.limite_extraccion = 50000
+        self.saldo = int(lineas[2].strip())
+        self.limite_extraccion = int(lineas[3].strip())
 
-        # registro de ops
+        # registro de operaciones
         self.historial = []
         self.contador_operaciones = 0
 
-    
-    # inicio sesion
-    
-    def iniciar_sesion(self):
-        usuario = input("Ingrese su usuario: ")
-        contrasena = input("Ingrese su contraseña: ")
+#inicio sesion
+def iniciar_sesion(self):
+    usuario = input("Ingrese su usuario: ")
+    contrasena = input("Ingrese su contraseña: ")
 
-        if usuario == self.usuario and contrasena == self.contrasena:
-            print("\nAcceso concedido.")
-            return True
+    if usuario == self.usuario and contrasena == self.contrasena:
+        print("\nAcceso concedido.")
+        return True
+    else:
+        print("\nUsuario o contraseña incorrectos.")
+        return False
+    
+#Consultar saldo
+def consultar_saldo(self):
+    print(f"\nSaldo disponible: ${self.saldo:.2f}")
+    self.historial.append("Consulta de saldo")
+    self.contador_operaciones += 1
+
+#deposito
+def depositar(self):
+    try:
+        monto = float(input("Ingrese el monto a depositar: $"))
+
+        if monto <= 0:
+            print("Error: El monto debe ser mayor que cero.")
         else:
-            print("\nUsuario o contraseña incorrectos.")
-            return False
+            self.saldo += monto
+            self.historial.append(f"Se han agregado a su cuenta: ${monto:.2f}")
+            self.contador_operaciones += 1
+            print("Deposito exitoso.")
 
-    # CONSULTO SALDO
-    
-    def consultar_saldo(self):
-        print(f"\nSaldo disponible: ${self.saldo:.2f}")
-        self.historial.append("Consulta de saldo")
-        self.contador_operaciones += 1
+    except ValueError:
+        print("Error: Ingrese numero valido.")
 
-    
-    # Deposito de plata
-    
-    def depositar(self):
-        try:
-            monto = float(input("Ingrese el monto a depositar: $"))
+#extraccion
+def extraer(self):
+    try:
+        monto = float(input("Ingrese el monto que desea extraer: $"))
 
-            if monto <= 0:
-                print("Error: El monto debe ser mayor que cero.")
-            else:
-                self.saldo += monto
-                self.historial.append(f"Se han agregado a su cuenta: ${monto:.2f}")
-                self.contador_operaciones += 1
-                print("Deposito exitoso.")
+        if monto <= 0:
+            print("Error: El monto debe ser mayor que cero.")
+        elif monto > self.saldo:
+            print("Error: Fondos insuficientes.")
+        elif monto > self.limite_extraccion:
+            print(f"Error: El monto excede el límite de extracción de ${self.limite_extraccion:.2f}.")
+        else:
+            self.saldo -= monto
+            self.historial.append(f"Se han retirado de su cuenta: ${monto:.2f}")
+            self.contador_operaciones += 1
+            print("Extracción exitosa.")
 
-        except ValueError:
-            print("Error: Ingrese numero valido.")
-
-    
-    # extraccion
-    
-    def extraer(self):
-        try:
-            monto = float(input("Ingrese el monto que desea extraer: $"))
-
-            if monto <= 0:
-                print("Error: El monto debe ser mayor que cero.")
-            elif monto > self.limite_extraccion:
-                print(
-                    f"Error: no puede extraer mas de ${self.limite_extraccion:.2f}."
-                )
-            elif monto > self.saldo:
-                print("Error: saldo insuficiente.")
-            else:
-                self.saldo -= monto
-                self.historial.append(f"Extraccion: ${monto:.2f}")
-                self.contador_operaciones += 1
-                print("Extraccion exitosa.")
-
-        except ValueError:
-            print("Error: Ingrese un numero valido.")
+    except ValueError:
+        print("Error: Ingrese numero valido.")
 
 #transferencia
 
@@ -93,6 +86,13 @@ def transferir(self):
 
     except ValueError:
         print("Error: Ingrese numero valido.")
+
+def guardar_datos(self):
+    with open("datos.txt", "w") as archivo:
+        archivo.write(f"{self.usuario}\n")
+        archivo.write(f"{self.contrasena}\n")
+        archivo.write(f"{self.saldo}\n")
+        archivo.write(f"{self.limite_extraccion}\n")        
 
 iniciar = input("¿Desea iniciar sesión? (s/n): ")
 if iniciar == 's':
@@ -119,6 +119,7 @@ if iniciar == 's':
                 transferir(cajero)
             elif opcion == '5':
                 print("Gracias por usar el cajero automático. ¡Hasta luego!")
+                guardar_datos(cajero)
                 break
             else:
                 print("Opción no válida. Por favor, intente nuevamente.")
